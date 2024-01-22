ce14dc56256e1f189a58314d39dc90e7397cc0eb
phattanaphong kiattipatarachai <frame2407@gmail.com>
2024-01-22 23:46:32 +0700

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TestMVC.DependencyInjection.@interface;
+using TestMVC.Models;
+using static TestMVC.Models.ProductDTO;
+
+namespace TestMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        IWebHostEnvironment _hostingEnvironment { get; set; }
+        IGlobalValueService _global { get; set; }
+        public ProductController( IWebHostEnvironment hostingEnvironment , IGlobalValueService global)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _global = global;
+        }
+        public IActionResult Index()
+        {
+            string PathMockup = Path.Combine(_hostingEnvironment.ContentRootPath, "Mockup", "Product");
+            string PathJson = Path.Combine(PathMockup, "Product.json");
+            var JsonPath = JsonFileReader.ReadAsync<ProductDTO.Root>(PathJson).Result;
+            _global.SetMockupValue(JsonPath);
+            return View(JsonPath);
+        }
+        public IActionResult Detail(int id)
+        {
+            var JsonFile = _global.GetMockupValue();
+            var findDetail = new ProductDTO.Product();
+            foreach (var item in JsonFile.products ?? new List<ProductDTO.Product>())
+            {
+                if (item.id == id) {
+                    findDetail = item;
+                    break;
+                }
+            }
+
+            var model = new ModelProduct()
+            {
+                currentImage = findDetail?.images?[0] ?? string.Empty,
+                currentIndex = 0,
+            };
+
+            return View(new ProductDTO.ModelDetail() { 
+                modelProduct = model , 
+                products= findDetail
+            });
+        }
+        public string ConvertNumberToComma(string value)
+        {
+            return value.Replace(",", ".");
+        }
+
+    }
+    public static class JsonFileReader
+    {
+        public static async Task<T> ReadAsync<T>(string filePath)
+        {
+            using FileStream stream = System.IO.File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(stream) ;
+        }
+    }
+}
---
+++ Models/product/ProductDTO.cs
@@ -0,0 +1,39 @@
+﻿namespace TestMVC.Models
+{
+    public class ProductDTO
+    {
+        public class Product
+        {
+            public int id { get; set; }
+            public string? title { get; set; }
+            public string? description { get; set; }
+            public int price { get; set; }
+            public double discountPercentage { get; set; }
+            public double rating { get; set; }
+            public int stock { get; set; }
+            public string? brand { get; set; }
+            public string? category { get; set; }
+            public string? thumbnail { get; set; }
+            public List<string>? images { get; set; }
+        }
+
+        public class Root
+        {
+            public List<Product>? products { get; set; }
+            public int total { get; set; }
+            public int skip { get; set; }
+            public int limit { get; set; }
+        }
+        public class ModelProduct
+        {
+            public string? currentImage { get; set; }
+            public int currentIndex { get; set; }
+        }
+
+        public class ModelDetail
+        {
+            public ModelProduct? modelProduct { get; set; }
+            public Product? products { get; set; }
+        }
+    }
+}
