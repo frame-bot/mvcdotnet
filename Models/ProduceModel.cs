435a1186594cd768ecc64814a3ec0e2cd3529413
phattanaphong kiattipatarachai <frame2407@gmail.com>
2024-01-23 23:56:07 +0700

    update git rm -r . --cached

--- Models/ProduceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestMVC.Models
-{
-    public class ProduceModel
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? ImagePath { get; set; }
-    }
-}
