cef10ac7a083b127db67bcb048c3fea28d58edae
phattanaphong kiattipatarachai <frame2407@gmail.com>
2024-02-01 23:23:30 +0700

    edit product add action create update and delete

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -22,15 +22,15 @@ namespace TestMVC.Controllers
 
             _httpContextAccessor.HttpContext?.Session.SetString("UserName", "admin");
             _httpContextAccessor.HttpContext?.Session.SetString("UserID", "admin");
-            var JsonPath = getProductByJson();
-            _global.SetMockupValue(JsonPath);
-            return View(JsonPath);
+            var JsonProductValue = getProductByJson();
+            _global.SetMockupValue(JsonProductValue);
+            return View(JsonProductValue);
         }
         public IActionResult Detail(int id)
         {
-            var JsonFile = _global.GetMockupValue();
+            var JsonProductValue = _global.GetMockupValue();
             var findDetail = new ProductDTO.Product();
-            foreach (var item in JsonFile?.products ?? new List<ProductDTO.Product>())
+            foreach (var item in JsonProductValue?.products ?? new List<ProductDTO.Product>())
             {
                 if (item.id == id)
                 {
@@ -41,7 +41,7 @@ namespace TestMVC.Controllers
 
             var model = new ModelProduct()
             {
-                currentImage = findDetail?.images?[0] ?? string.Empty,
+                currentImage = string.Empty,
                 currentIndex = 0,
             };
 
@@ -65,25 +65,113 @@ namespace TestMVC.Controllers
             return Json(getJson);
         }
 
+        [HttpPost]
+        public IActionResult addItem([FromBody] Product value)
+        {
+            var JsonProductValue = getProductByJson();
+
+            // find max id
+            int newId = 0;
+            foreach (var item in JsonProductValue?.products ?? new List<ProductDTO.Product>())
+            {
+                if (item.id > newId)
+                {
+                    newId = item.id;
+                }
+            }
+
+            value.id = newId + 1;
+            JsonProductValue?.products?.Add(value);
+
+            // save to json
+            string PathJson = getPathProductJson();
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            var jsonString = JsonSerializer.Serialize(JsonProductValue, options);
+            System.IO.File.WriteAllText(PathJson, jsonString);
+            _global.SetMockupValue(JsonProductValue ?? new ProductDTO.Root());
+            return Json(value);
+        }
+
+        [HttpPut]
+        public IActionResult updateItem([FromBody] Product value)
+        {
+            var JsonProductValue = getProductByJson();
+
+            // find value and update to json
+            foreach (var item in JsonProductValue?.products ?? new List<ProductDTO.Product>())
+            {
+                if (item.id == value.id)
+                {
+                    item.title = value.title;
+                    item.description = value.description;
+                    item.price = value.price;
+                    item.discountPercentage = value.discountPercentage;
+                    item.rating = value.rating;
+                    item.stock = value.stock;
+                    item.brand = value.brand;
+                    item.category = value.category;
+                    item.thumbnail = value.thumbnail;
+                    item.images = value.images;
+                    break;
+                }
+            }
+
+            // save to json
+            string PathJson = getPathProductJson();
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var jsonString = JsonSerializer.Serialize(JsonProductValue, options);
+            System.IO.File.WriteAllText(PathJson, jsonString);
+            _global.SetMockupValue(JsonProductValue ?? new ProductDTO.Root());
+            return Json(value);
+        }
+
+        [HttpDelete]
+        public IActionResult deleteItem(int id)
+        {
+            var JsonProductValue = getProductByJson();
+
+            // find value and update to json
+            foreach (var item in JsonProductValue?.products ?? new List<ProductDTO.Product>())
+            {
+                if (item.id == id)
+                {
+                    JsonProductValue?.products?.Remove(item);
+                    break;
+                }
+            }
+
+            // save to json
+            string PathJson = getPathProductJson();
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            var jsonString = JsonSerializer.Serialize(JsonProductValue, options);
+            System.IO.File.WriteAllText(PathJson, jsonString);
+            _global.SetMockupValue(JsonProductValue ?? new ProductDTO.Root());
+            return Json(id);
+        }
+
         private ProductDTO.Root getProductByJson()
         {
-
-            string PathMockup = Path.Combine(_hostingEnvironment.ContentRootPath, "Mockup", "Product");
-            string PathJson = Path.Combine(PathMockup, "Product.json");
+            string PathJson = getPathProductJson();
             var JsonPath = JsonFileReader.ReadAsync<ProductDTO.Root>(PathJson).Result;
             return JsonPath;
         }
 
-        // [HttpPost]
-        // public JsonResult setCurrentImageValue([FromBody] PostSetCurrentImageValue value)
-        // {
-        //     TempData["currentImage"] = value.image;
-        //     return Json(value);
-        // }
-        // public string ConvertNumberToComma(string value)
-        // {
-        //     return value.Replace(",", ".");
-        // }
+        private string getPathProductJson()
+        {
+            string PathMockup = Path.Combine(_hostingEnvironment.ContentRootPath, "Mockup", "Product");
+            string PathJson = Path.Combine(PathMockup, "Product.json");
+            return PathJson;
+        }
 
     }
     public static class JsonFileReader
--- Models/product/ProductDTO.cs
+++ Models/product/ProductDTO.cs
@@ -7,10 +7,10 @@
             public int id { get; set; }
             public string? title { get; set; }
             public string? description { get; set; }
-            public int price { get; set; }
-            public double discountPercentage { get; set; }
-            public double rating { get; set; }
-            public int stock { get; set; }
+            public int? price { get; set; }
+            public double? discountPercentage { get; set; }
+            public double? rating { get; set; }
+            public int? stock { get; set; }
             public string? brand { get; set; }
             public string? category { get; set; }
             public string? thumbnail { get; set; }
